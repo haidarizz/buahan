14c6270d313ab5bbf8b420b5b8c6bb8dea3eb374
haidarizz <mhibyl@gmail.com>
2023-07-11 13:27:33 +0800

    feat: navbar

---
+++ src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-links",
+        icon : "fa-solid fa-house-user"
+    },
+    {
+        title : "Tentang",
+        url : "/tentang",
+        cName : "nav-links",
+        icon : "fa-solid fa-circle-info"
+    },
+    {
+        title : "Wisata",
+        url : "/wisata",
+        cName : "nav-links",
+        icon : "fa-solid fa-clipboard"
+    },
+    {
+        title : "Kegiatan",
+        url : "/kegiatan",
+        cName : "nav-links",
+        icon : "fa-solid fa-users"
+    },
+    {
+        title : "Peta",
+        url : "/peta",
+        cName : "nav-links",
+        icon : "fa-solid fa-image"
+    }
+]
